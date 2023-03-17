fe404dcb009c80bcf3bb1b9e850a0a8cfcf9cae0
nomaprof <nomaprof@gmail.com>
2023-03-16 21:00:36 -0700

    Update 15

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - entry point
- * return: Always 0 (success)
+ *
+ * return: 0
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
