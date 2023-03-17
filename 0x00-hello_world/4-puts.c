0d92f84bacb079dea20a794ed1aee9efd30c7d87
nomaprof <nomaprof@gmail.com>
2023-03-16 19:49:05 -0700

    Update 11

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /*
- * Use the function puts to print to standard output
+ * main -  Use the function puts to print to standard output
  *
  * return (0)
  */
 
-int main (void)
+int main(void)
 
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
