0d0e831655aadf58b2690d594cae80745e230f3c
nomaprof <nomaprof@gmail.com>
2023-03-17 19:18:44 -0700

    Update 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int base10;
+
+	for (base10 = 0; base10 < 10; base10++)
+	{
+		printf("%d\n", base10);
+	}
+	return (0);
+}
