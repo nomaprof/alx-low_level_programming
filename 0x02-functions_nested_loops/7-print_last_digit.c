b0e02c4e898ad1e94c00bb81e611084cb09b3a01
nomaprof <nomaprof@gmail.com>
2023-03-23 06:01:41 -0700

    correction1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int r)
 	int n;
 
 	if (r < 0)
-		n = -1 * (r);
+		n = -1 * (r%10);
 	else
-		n = r;
+		n = r%10;
 
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	_putchar(n + '0');
+	return (n);
 }
 
