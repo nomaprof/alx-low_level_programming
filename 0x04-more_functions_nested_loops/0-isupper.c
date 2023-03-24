3b3054efe48a8a7ad4c418ca11a3ba3ae93b4ce0
nomaprof <nomaprof@gmail.com>
2023-03-23 21:57:42 -0700

    continebreak

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
+int _isupper(int c)
+{
+	char ch;
+	int isupper;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		if (c == ch)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+	return (isupper);
+}
