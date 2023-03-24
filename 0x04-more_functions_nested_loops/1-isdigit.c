84a7ab840515cc1ae61be4ea0de377168f2821e9
nomaprof <nomaprof@gmail.com>
2023-03-24 00:35:50 -0700

    isadigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+*_isdigit - function that verifies if a  character is a digit or not
+*@c: tested character
+*Return: returns 1 if it is a digit, 0 if not
+*/
+
 int _isdigit(int c)
 
 {
-	if ((c >= 0) && (c >= 9))
-	return (1);
-
+	for (c = 0; c <= 9; c++)
+	{
+		return (1);
+	}
 	return (0);
 }
