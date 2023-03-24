da2c2319066776290ac9869c0443f56a09109c44
nomaprof <nomaprof@gmail.com>
2023-03-24 01:45:59 -0700

    omitsomenumbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_most_numbers - prints numbers except 2 and 4
+*
+*Return: returns nothing
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+	char num[8] = "01356789";
+
+	for (i = 0; i >= 7; i++)
+	{
+		_putchar(num[i]);
+	}
+}
