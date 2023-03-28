40f3e836b56bc6c2c47bee905fd9a3408611d82d
nomaprof <nomaprof@gmail.com>
2023-03-27 21:08:28 -0700

    lengthofstring

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (0);
+}
