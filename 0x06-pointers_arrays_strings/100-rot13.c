6a4941603d157eaade5c23282b7126a83abf1841
nomaprof <nomaprof@gmail.com>
2023-03-30 17:38:36 -0700

    moreencryption

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,14 +2,44 @@
 
 /**
 * rot13 -  encodes a string using rot13
-* @str:the string targeted
+* @s:the string targeted
 *Return: returns the encoded string
 */
 
 char *rot13(char *s)
 {
-	int i;
+	int i, j;
 
-	while (s[i] != '\0')
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+											 'G', 'H', 'I', 'J', 'K', 'L',
+											 'M', 'N', 'O', 'P', 'Q', 'R',
+											 'S', 'T', 'U', 'V', 'W', 'X',
+											 'Y', 'Z', 'a', 'b', 'c', 'd',
+											 'e', 'f', 'g', 'h', 'i', 'j',
+											 'k', 'l', 'm', 'n', 'o', 'p',
+											 'q', 'r', 's', 't', 'u', 'v',
+											 'w', 'x', 'y', 'z'};
+
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+											 'T', 'U', 'V', 'W', 'X', 'Y',
+											 'Z', 'A', 'B', 'C', 'D', 'E',
+											 'F', 'G', 'H', 'I', 'J', 'K',
+											 'L', 'M', 'n', 'o', 'p', 'q',
+											 'r', 's', 't', 'u', 'v', 'w',
+											 'x', 'y', 'z', 'a', 'b', 'c',
+											 'd', 'e', 'f', 'g', 'h', 'i',
+											 'j', 'k', 'l', 'm'};
+	while (s[++i])
 	{
-		if (i = 0; ((i >= 'a' && i <= 'z')||(i >= 'A' && i <= 'Z'));   
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alphabet[j])
+			{
+				s[i] = rot13key[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
