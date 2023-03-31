61d41aa4fec454b28ae1a1380f9132d2d970892d
nomaprof <nomaprof@gmail.com>
2023-03-30 18:55:07 -0700

    moreattempt

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -20,4 +20,5 @@ char *rot13(char *);
 int check_seperators(char c);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif /*MAIN_H*/
