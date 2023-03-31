e2e04116ac20c6a4ec78cb812365f50030e9010e
nomaprof <nomaprof@gmail.com>
2023-03-30 17:11:41 -0700

    comparison

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* rot13 -  encodes a string using rot13
+* @str:the string targeted
+*Return: returns the encoded string
+*/
+
+char *rot13(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		if (i = 0; ((i >= 'a' && i <= 'z')||(i >= 'A' && i <= 'Z'));   
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,30 +12,30 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, sum;
+	int i = 0, diff = 0;
 
-	for (i = 0; s1[i] != '\0'; ++i)
+	while (1)
 	{
-	}
-	for (j = 0; s2[j] != '\0'; ++j)
-	{
-	}
-	if (i < j)
-	{
-		sum = (-(i + j));
-	}
-	else if (i > j)
-	{
-		sum = (i + j);
-	}
-	else if (i == j)
-	{
-		sum = (i - j);
-	}
-	else
-	{
-		sum = 0;
-	}
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 
-	return (sum);
+	}
+	return (diff);
 }
