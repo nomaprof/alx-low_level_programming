71b97eae268ba248012304d70ef1e46d78515cd1
nomaprof <nomaprof@gmail.com>
2023-04-03 21:31:48 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
-*_memset - The _memset() function fills
-* the first n bytes of the memory area
-* pointed to by s with the constant byte b
-*@s:target
-*@b: constant byte
-*@n:number of byte
-*Return: returns new value of target
+*_memset - The function to modify buffer
+*@s: pointer to location
+*@b: value to be entered
+*@n: no of bytes to be changed
+*Return: returns the new value (s)
 */
 
 char *_memset(char *s, char b, unsigned int n)
