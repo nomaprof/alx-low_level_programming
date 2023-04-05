39de4bc2019d2752993b4ac063564f78ae87788f
nomaprof <nomaprof@gmail.com>
2023-04-05 02:33:11 -0700

    haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*_strstr - Find the needle in the haystack
+*@haystack: Reference string
+*@needle: The string hid in the haystack
+*Return: A pointer to the start of found substring
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int m, n;
+
+	for (m = 0; haystack[m] != '\0'; m++)
+	{
+		for (n = 0; needle[n] != '\0'; n++)
+		{
+			while (*haystack)
+			{
+				if (needle[n] == haystack[m])
+				{
+					return (haystack);
+				}
+
+				haystack++;
+			}
+		}
+	}
+
+	return (NULL);
+}
