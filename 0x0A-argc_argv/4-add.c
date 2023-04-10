bcedd913e5fd3e3d40d28c0c15e80b695c20c29b
nomaprof <nomaprof@gmail.com>
2023-04-10 10:46:47 -0700

    addnumbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - add positive mumbers
+ *
+ * @argc: count number of arguments
+ *
+ * @argv: arrary where arguments are stored
+ *
+ * Return: 1 if an argument is not an int
+ */
+
+int main(int argc, char *argv[])
+{
+	int m, ans = 0;
+
+	if (argc > 1)
+	{
+		for (m = 1; m < argc; m++)
+		{
+			if (*argv[m] < '0' || *argv[m] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		
+			{
+				ans += atoi(argv[m]);
+				printf("%d\n", ans);
+			}
+		}
+	}
+	else if (argc == 1)
+	{
+		printf("0\n");
+	}
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
 * void prototypes()
