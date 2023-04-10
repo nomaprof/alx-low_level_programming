f71b95d62ad5737ccb26c7498322a857b1ccf85c
nomaprof <nomaprof@gmail.com>
2023-04-10 08:18:11 -0700

    numberofarguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print the number of arguments
+ *
+ * @argc: gives the number of arguments
+ *
+ * @argv: gives the array of strings
+ *
+ * Return: returns 0 if successful
+ */
+
+int main(int argc, char __attribute__((__unused__))*argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
