a6b7443d4ad6d36359c9d74ac88269e7579dc557
nomaprof <nomaprof@gmail.com>
2023-04-10 05:47:13 -0700

    writetheprogramname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - whats the program name
+*@argc: number of command line arguments
+*@argv: the place the arguments are stored
+*Return: 0
+*/
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+* void prototypes()
+*char prototypes()
+* int prototypes()
+*/
+
+int _putchar(char c);
+#endif /*MAIN_H*/
