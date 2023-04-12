f2d3f8e14680f2534772967a9fa80c667ba9b1dc
nomaprof <nomaprof@gmail.com>
2023-04-12 01:45:39 -0700

    corrections

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,14 +1,10 @@
 #include "main.h"
 /**
  * main - make change
- *
  * @argc: number of arguments
- *
  * @argv: array containing arguments
- *
  * Return: 0 if the program is successful
  */
-
 int main(int argc, char *argv[])
 {
 	int a, ans = 0;
@@ -17,40 +13,26 @@ int main(int argc, char *argv[])
 	{
 		a = atoi(argv[1]);
 		if (a < 0)
-		{
 			printf("%d\n", 0);
 			return (0);
-		}
 		if (a % 25 >= 0)
-		{
 			ans += a / 25;
 			a = a % 25;
-		}
 		if (a % 10 >= 0)
-		{
 			ans += a / 10;
 			a = a % 10;
-		}
 		if (a % 5 >= 0)
-		{
 			ans += a / 5;
 			a =  a % 5;
-		}
 		if (a % 2 >= 0)
-		{
 			ans += a / 2;
 			a = a % 2;
-		}
 		if (a % 1 >= 0)
-		{
 			ans += a / 1;
 			printf("%d\n", ans);
 			return (0);
-		}
 	}
 	else
-	{
 		printf("Error\n");
 		return (1);
-	}
 }
