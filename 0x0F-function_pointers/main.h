57c2003fae5b0eb04ebf41d0819e0fb2add43298
nomaprof <nomaprof@gmail.com>
2023-04-20 04:49:23 -0700

    functionpointers

---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+* void prototypes()
+*char prototypes()
+* int prototypes()
+*/
+
+void _puts_recursion(char *s);
+ void _print_rev_recursion(char *s);
+int _putchar(char c);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int ans(int n, int m);
+int prime(int n, int m);
+int len(char *s);
+int answer(char *s, int n, int m);
+#endif /*MAIN_H*/
