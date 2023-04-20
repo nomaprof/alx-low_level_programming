13b6c15cb6c11425827f959b01f820ed64b9de5d
nomaprof <nomaprof@gmail.com>
2023-04-20 06:46:07 -0700

    functionpointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract numbers
+ * @a: first number
+ * @b: second number
+ * Return: substraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide numbers
+ * @a: first number
+ * @b: second number
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_mod - remainder after division
+ * @a: first number
+ * @b: second number
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
