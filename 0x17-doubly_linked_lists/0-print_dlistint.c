2a98b37837658d489cefe0355f51a4fdbd54798e
nomaprof <nomaprof@gmail.com>
2023-06-16 02:31:49 -0700

    Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,32 +1,30 @@
 #include "lists.h"
 
 /**
- * print_dlistint - get the number of elements in a doublylinked list
- * @h: pointer to start
- *
- * Return: the number of elements in the doublylinked list
- */
+  * print_dlistint - print members of doubly linked lists
+  * @h: pointer to start of doubly linked lists
+  *
+  * Return: the total number of nodes in struct.
+  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    int tracker = 0;
+	int tracker = 0;
 
-    if (h == NULL)
-    {
-        return (tracker);
-    }
+	if (h == NULL)
+	{
+		return (tracker);
+	}
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		tracker++;
+	}
 
-    while (h->prev != NULL)
-    {
-        h = h->prev;
-    }
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        tracker++;
-    }
-
-    return (tracker);
+	return (tracker);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -5,19 +5,18 @@
 #include <stdlib.h>
 
 /**
- * struct dlistint_s - list with nodes able to point to next and previous
- * @n: the valued of interest
- * @prev: pointer to previous
- * @next: pointer to next
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
  *
- * Description: a double linked list
- * 
+ * Description: doubly linked list node structure
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
