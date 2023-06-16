01b8b1c16d45e2fdc676d6a225cb5c24d687fe62
nomaprof <nomaprof@gmail.com>
2023-06-16 03:14:08 -0700

    Doubly linked lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,25 +1,23 @@
 #include "lists.h"
 
 /**
- *dlistint_len - get the number of elements
- *@h: ponter to start
- *
- *Return: the exact number of nodes
- */
+  * dlistint_len - get number of nodes
+  * @h: point to start of list
+  *
+  * Return: Success or Failure
+  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    int tracker = 0;
+	int tracker = 0;
 
-    if (h == NULL)
-    {
-        return (tracker);
-    }
-
-    while (h != NULL)
-    {
-        tracker++;
-        h = h->next;
-    }
-
-    return (tracker);
+	if (h == NULL)
+	{
+		return (tracker);
+	}
+	while (h != NULL)
+	{
+		tracker++;
+		h = h->next;
+	}
+	return (tracker);
 }
