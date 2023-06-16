8a0c992adb71db6a89bf4f9bf1892a7e43285b73
nomaprof <nomaprof@gmail.com>
2023-06-16 04:27:40 -0700

    Doubly linked lists

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+  * insert_dnodeint_at_index - place a new element a particular index
+  * @h: pointer to start of list
+  * @n: value to be inserted
+  *
+  * Return: Success or Failure
+  */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}
