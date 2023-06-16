fc8356cee807db67f53d555ea91878847ac43d72
nomaprof <nomaprof@gmail.com>
2023-06-16 03:43:51 -0700

    Doubly linked lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,40 +1,40 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - place new node at the end of the doubly linked list
- *@head: pointer to the start of the doubly linked list
- *
- *Return: the address of new element, or NULL if it failed
- */
+  * add_dnodeint_end - add a node to the end of the list
+  * @head: pointer to start of the lisht
+  * @n: value to be inserted at the end of the list
+  *
+  * Return: Success or Failure
+  */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-   dlistint_t *insert_new;
+	dlistint_t *insert_new;
 
-   insert_new = malloc(sizeof(dlistint_t));
+	insert_new = malloc(sizeof(dlistint_t));
 
-   if (insert_new == NULL)
-   {
-      return (NULL);
-   }
+	if (insert_new == NULL)
+	{
+		return (NULL);
+	}
+	insert_new->n = n;
+	insert_new->next = NULL;
 
-   insert_new->n = n;
-   insert_new->next = NULL;
+	if (*head == NULL)
+	{
+		insert_new->prev = NULL;
+		*head = insert_new;
+		return (insert_new);
+	}
 
-   if (*head == NULL)
-   {
-      insert_new->prev = NULL;
-      *head = insert_new;
-      return (insert_new);
-   }
+	while ((*head)->next != NULL)
+	{
+		*head = (*head)->next;
+	}
 
-   while ((*head)->next != NULL)
-   {
-      *head = (*head)->next;
-   }
+	(*head)->next = insert_new;
+	insert_new->prev = *head;
 
-   (*head)->next = insert_new;
-   insert_new->prev = *head;
-
-   return (insert_new);
+	return (insert_new);
 }
